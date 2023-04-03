8f955d7972d39c93296ca2eefa55d27b9412493b
Jongho Han <jongho.han@voyagerx.com>
2023-04-03 15:21:58 +0900

    Fix stopwatch

--- ts/src/timing/stopwatch.ts
+++ ts/src/timing/stopwatch.ts
@@ -1,58 +1,65 @@
+import { sum } from "../data/stat";
+
 export class StopWatch {
-  private elapsed: number[];
-  private elapsedLaps: number[];
-  private lastStarted: number | undefined;
-  private lastLap: number | undefined;
+  public elapsed: number[];
+  public elapsedLaps: number[];
+  private last: {
+    started: number;
+    lap: number;
+  } | undefined;
 
   constructor() {
     this.elapsed = [];
     this.elapsedLaps = [];
-    this.lastStarted = Date.now();
-    this.lastLap = Date.now();
+    this.last = {
+      started: Date.now(),
+      lap: Date.now(),
+    }
   }
 
   public pause = () => {
-    if (this.lastStarted === undefined || this.lastLap === undefined) {
+    if (this.last === undefined) {
       return;
     }
 
-    this.elapsed.push(Date.now() - this.lastStarted);
-    this.elapsedLaps.push(Date.now() - this.lastLap);
-    this.lastStarted = undefined;
-    this.lastLap = undefined;
+    this.elapsed.push(Date.now() - this.last.started);
+    this.last = undefined;
   };
 
   public resume = () => {
-    if (this.lastStarted !== undefined || this.lastLap !== undefined) {
+    if (this.last !== undefined) {
       return;
     }
 
     const resumedTime = Date.now();
-    this.lastStarted = resumedTime;
-    this.lastLap = resumedTime;
+    this.last = {
+      started: resumedTime,
+      lap: resumedTime,
+    }
   };
 
   public reset = () => {
     this.elapsed = [];
     this.elapsedLaps = [];
-    this.lastStarted = Date.now();
-    this.lastLap = Date.now();
+    this.last = {
+      started: Date.now(),
+      lap: Date.now(),
+    }
   };
 
   public getElapsedSec = (): number => {
-    const prevElapsed = this.elapsed.reduce((a, b) => a + b, 0);
+    const prevElapsed = sum(this.elapsed);
     const currElapsed =
-      this.lastStarted === undefined ? 0 : Date.now() - this.lastStarted;
+      this.last === undefined ? 0 : Date.now() - this.last.started;
     return (prevElapsed + currElapsed) / 1000;
   };
 
-  public lap = (): number => {
-    const prevElapsed = this.elapsedLaps.reduce((a, b) => a + b, 0);
-    const currElapsed =
-      this.lastLap === undefined ? 0 : Date.now() - this.lastLap;
-
-    this.elapsedLaps = [];
-    this.lastLap = Date.now();
-    return (prevElapsed + currElapsed) / 1000;
+  public lap = (): void => {
+    if (this.last === undefined) {
+      return;
+    }
+    const now = Date.now();
+    this.elapsedLaps.push(now - this.last.lap);
+    this.last.lap = now;
   };
 }
