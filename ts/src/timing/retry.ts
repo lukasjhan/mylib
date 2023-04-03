4d7d445822f95733153bdb9ed656c44141b39c90
Jongho Han <jongho.han@voyagerx.com>
2023-04-03 10:39:15 +0900

    Add waitTime in retry

--- ts/src/timing/retry.ts
+++ ts/src/timing/retry.ts
@@ -3,14 +3,15 @@ import { sleep } from "./timeout";
 
 export async function retry<T>(
   fn: () => Promise<T>,
-  retries: number
+  retries: number,
+  waitTimeMs: number = 500,
 ): Promise<T> {
   for (const _ of range(retries - 1)) {
     try {
       const result = await fn();
       return result;
     } catch (err) {
-      await sleep(500);
+      await sleep(waitTimeMs);
     }
   }
   return await fn();
